4b475359093093b9e14e509600f348322fa4f1a9
raghuponde <33595609+raghuponde@users.noreply.github.com>
2025-01-18 09:40:31 +0530

    ssss

--- sample.cs
+++ sample.cs
@@ -1,2 +1,55 @@
 
-hai 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propertiesdemo
+{
+
+
+    class Customer
+    {
+        private int _Id;
+        private string _CustomerName;
+        public int ID
+        {
+            set
+            {
+                _Id = value;
+            }
+            get
+            {
+                return _Id;
+            }
+        }
+
+        public string CustomerName1
+        {
+            set
+            {
+                _CustomerName = value;
+            }
+            get
+            {
+                return _CustomerName;
+            }
+        }
+
+
+    }
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer  cobj = new Customer();
+            cobj.ID = 101;
+            cobj.CustomerName1 = "ravi";
+            Console.WriteLine($"{cobj.CustomerName1} {cobj.ID}");
+            Console.ReadLine();
+            
+        }
+    }
+}
+
